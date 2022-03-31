c2870888f21c705325ca982bc17d3dd1c4b5038e
ShubhamHule <97870834+ShubhamHule@users.noreply.github.com>
2022-03-31 17:01:13 +0530

    Create HelloRestController.java

---
+++ src/main/java/com/zensar/webapp/HelloRestController.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class HelloRestController{
+  @GetMapping("/Hello")
+  public String getmessage()
+  {
+    return "Welcome to Devops";
+  }
+}
